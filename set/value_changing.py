da0efcbfc447a37c06e9b055e1553b0139aa79b1
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-05 19:55:54 +0530

    set adding and joining iterable objects

---
+++ set/value_changing.py
@@ -0,0 +1,23 @@
+names = {'hisham', 'dulfacker', 'liya', 'filsor'}
+
+print(names)
+
+names.add("fathima")
+print(names)
+
+name_1 = {"izu", "nidal"}
+
+names.update(name_1)
+print(names)
+
+"""adding another iterable python object"""
+my_list = [123,465,789]
+my_dic = {"number": 123,
+          "faa": "foo"
+          }
+
+names.update(my_list)
+print(names)
+
+names.update(my_dic)
+print(names)
