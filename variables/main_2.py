0146dc486ade41fabfd4cd93469897276965559b
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-05 10:43:17 +0530

    unpacking

---
+++ variables/main_2.py
@@ -0,0 +1,16 @@
+# unpacking a collection
+
+
+"""here i need to give each cars to its corresponding company"""
+cars = ["mustang", "m3", "AMG"]
+print(cars)
+
+ford, bmw, mercedez = cars
+print(ford)
+print(bmw)
+print(mercedez)
+
+#printing variable name along with values
+print(f"{ford=}")
+print(f"{bmw=}")
+print(f"{mercedez=}")
