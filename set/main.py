b5b951773f0fd62aac987a7625c6e7a3b52e2896
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-05 19:35:51 +0530

    set basics

---
+++ set/main.py
@@ -0,0 +1,16 @@
+myset = {}
+print(type(myset))
+
+mmyset = set()
+print(type(mmyset))
+
+a = {True,1,2,3,4,5,1}
+print(a)
+
+n = set()
+n.add(True)
+n.add(1)
+print(f"n var => {n}")
+
+p = {1,2,3,1,1,2,5,6}
+print(p)
