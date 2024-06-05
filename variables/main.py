fb45f966c41c83601e208262840185b73238c3a9
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-05 10:31:40 +0530

    variable basics

---
+++ variables/main.py
@@ -0,0 +1,17 @@
+a = "hisham"
+print(type(a))
+
+a = "kardaash"
+
+A = "the change is on"
+print(A, a)
+
+multiple_words = "should be separated by underscore"
+
+
+THIS_CONSTANT_VARIABLES =   "as per the pep8 rule developers are just considering this as constant variables , its just a naming convention"
+
+"""
+_ => temporary variable ,check it out
+
+"""
