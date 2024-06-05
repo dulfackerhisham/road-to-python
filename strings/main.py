37dacb15db5e6f652a52f8de89f80d9aedd13850
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-05 12:01:00 +0530

    string and slicing

---
+++ strings/main.py
@@ -0,0 +1,17 @@
+test = "Hello, World"
+t = "doesn\'t' know"
+
+# print(len(test))
+# print(t)
+
+# if "h" in test:
+#     print(True)
+# else:
+#     print(False)
+
+"""string slicing"""
+print(test[:])
+print(test[::2])
+print(test[:6])
+print(test[3:9])
+print(test[-1])
