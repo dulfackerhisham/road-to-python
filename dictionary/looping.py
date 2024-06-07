5fbc68c6e968c98a9897e337bdb3032bf7b44eba
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-07 14:49:37 +0530

    looping through dictionary

---
+++ dictionary/looping.py
@@ -0,0 +1,28 @@
+my_dict = {
+    "name": "hisham",
+    "age": 23,
+    "place": "koyilandy",
+    "year": 2000,
+}
+
+
+"""printing out values"""
+# for x in my_dict:
+#     print(my_dict[x])
+
+# for x in my_dict.values():
+#     print(x)
+
+"""printing out keys"""
+# for i in my_dict:
+#     print(i)
+
+# for x in my_dict.keys():
+#     print(x)
+
+"""looping through key & values"""
+# for x in my_dict.items():
+#     print(x)
+
+for x, y in my_dict.items():
+    print(x, y)
